0728cb1e00fb086ee5b4ad44e3b5f5b764efdb5d
Yelena <lena.skripnik.01@gmail.com>
2021-03-07 18:28:16 +0200

    Create main_axis.dart

---
+++ Part_1/alignments/main_axis.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My first app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.star_border,
+                color: Colors.indigo,
+                size: 30.0,
+              ),
+              Icon(
+                Icons.all_inclusive_sharp,
+                color: Colors.cyan,
+                size: 40.0,
+              ),
+              Icon(
+                Icons.star_border,
+                color: Colors.indigo,
+                size: 30.0,
+              ),
+              Icon(
+                Icons.all_inclusive_sharp,
+                color: Colors.cyan,
+                size: 40.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
