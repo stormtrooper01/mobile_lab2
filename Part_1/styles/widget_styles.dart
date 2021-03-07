77843a219fdceee1ec64236184fc07acfdff3aa3
Yelena <lena.skripnik.01@gmail.com>
2021-03-07 18:13:49 +0200

    Create widget_styles.dart

---
+++ Part_1/styles/widget_styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My first app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Center(
+          child: Container(
+            width: 150.0,
+            height: 100.0,
+            decoration: BoxDecoration(
+              color: Colors.pinkAccent,
+              boxShadow: [
+                BoxShadow(
+                  offset: const Offset(3.0, 3.0),
+                  blurRadius: 5.0,
+                  spreadRadius: 2.0,
+                )
+              ]
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
